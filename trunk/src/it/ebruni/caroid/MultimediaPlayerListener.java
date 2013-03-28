64c84bdd7d962049928fdfa9e470138376db91fb
info@ebruni.it <info@ebruni.it@cab00883-5d9b-f919-d083-e82c68812059>
2013-03-28 17:27:47 +0000

    errore di import

--- trunk/src/it/ebruni/caroid/MultimediaPlayerListener.java
+++
@@ -1,7 +0,0 @@
-package it.ebruni.caroid;
-
-public interface MultimediaPlayerListener  {
-	public void onMediaStart(MultimediaPlayer mp);
-	public void onMediaPause(MultimediaPlayer mp);
-	public void onMediaComplete(MultimediaPlayer mp);
-}
